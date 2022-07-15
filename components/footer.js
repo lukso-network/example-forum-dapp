283af6f383231ead92bacd9aeda4e803b0f91730
fhildeb <felix@lukso.io>
2022-07-15 09:01:09 +0200

    add footer

---
+++ components/footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <a
+        className="Link"
+        href="https://wwww.github.com/lukso-network/example-forum-dapp"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        github.com/lukso-network/example-forum-dapp
+      </a>
+    </footer>
+  );
+}
