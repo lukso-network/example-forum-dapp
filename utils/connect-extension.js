04a1a08d7944e8c3c0cac7188942449e778c115f
fhildeb <felix@lukso.io>
2022-07-22 21:25:43 +0200

    add navigator share for security check

--- utils/connect-extension.js
+++ utils/connect-extension.js
@@ -2,6 +2,9 @@ import Web3 from 'web3';
 
 export async function connectWeb3() {
   // Check if Browser is Chrome or Firefox
+  if (navigator.share) {
+    setNativeShare(true);
+  }
   if (
     navigator.userAgent.indexOf('Firefox') !== -1 ||
     navigator.userAgent.indexOf('Chrome') !== -1
