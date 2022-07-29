1e152945ebabecc11520de44eccda9bd8f28dda6
fhildeb <felix@lukso.io>
2022-07-29 20:40:42 +0200

    add current state of ipfs upload

--- pages/create.js
+++ pages/create.js
@@ -4,6 +4,9 @@ import { Footer, Notifications } from '../components';
 import Link from 'next/link';
 import Profile from '../components/profile';
 import { GlobalContext } from '../contexts/GlobalContext';
+// before
+// import * as IPFS from 'ipfs-core';
+import * as IPFS from 'ipfs';
 
 function CreatePost() {
   const [blogpost, setBlockpostValues] = useState({
@@ -36,13 +39,53 @@ function CreatePost() {
     if (!ethereum) {
       alert('Please install Universal Profile Extension or Meth');
     }
+    console.log(JSON.stringify(blogpost));
+    // Connect with IPFS node
+    const node = await IPFS.create();
 
     // Upload Title and Text to IPFS
     try {
+      if (node.isOnline()) {
+        const IPFSPost = {
+          path: '/tmp/post.txt', // imaginary filename
+          content: JSON.stringify(blogpost),
+        };
+
+        console.log('starting post upload');
+        const result = await node.add(IPFSPost);
+
+        //console.log('pinning file...');
+        //await node.pin.add(result.cid);
+
+        const cid = result.cid.toString();
+        console.log('IPFS upload complete: ', cid);
+      } else {
+        console.log('Cannot connect with IPFS node');
+      }
+
+      /*
+      const data = 'Hello, <YOUR NAME HERE>';
+
+      // add your data to IPFS - this can be a string, a Buffer,
+      // a stream of Buffers, etc
+      const results = node.add(data);
+
+      // we loop over the results because 'add' supports multiple
+      // additions, but we only added one entry here so we only see
+      // one log line in the output
+      for await (const { cid } of results) {
+        // CID (Content IDentifier) uniquely addresses the data
+        // and can be used to get it again.
+        console.log(cid.toString());
+        
+      }*/
     } catch (error) {
       console.warn('Failed IPFS Upload:', error);
+      await node.stop();
+      console.log('IPFS node stopped');
     }
 
+    return;
     try {
       const tx = await LSP7Contract.methods
         .createPost(blogpost.title, blogpost.text)
