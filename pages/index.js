a6a3d40f7b28df37fb510a4e7952ee132d03f8bd
fhildeb <felix@lukso.io>
2022-08-12 21:45:53 +0200

    add index file

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+    console.log('index aufgerufen');
+  }, []);
+
+  return <></>;
+}
+
+export default Index;
